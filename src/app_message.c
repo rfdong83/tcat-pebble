8e015619f129f97e0f6da450f5bf1861ce6e3ce1
ajzwu8 <ajw238@cornell.edu>
2015-09-20 01:01:01 -0400

    clean up stop window

--- src/app_message.c
+++ src/app_message.c
@@ -4,33 +4,96 @@
  */
 
 #include <pebble.h>
-
+#include <stdio.h>
+  
 #define KEY_DATA 5
 
-static Window *s_main_window,*menu_window;
-static TextLayer *s_output_layer;
+static Window *s_main_window,*menu_window,*stop_window;
+static TextLayer *s_output_layer,*stop_text_layer;
 static SimpleMenuLayer *menu_layer;
 static SimpleMenuSection sections[1];
 static SimpleMenuItem items[5];
+static char stops[5][30];
+static char sched[5][100];
 
 
 // APP MESSAGE---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
+static void request_bus_data() {
+  // Get time and weekday to give to phone
+  time_t temp = time(NULL); 
+  struct tm *tick_time = localtime(&temp);
+  char weekday[18];
+  char time[10];
+  switch (tick_time->tm_wday) {
+    case 0:
+      strcpy(weekday,"Sunday");
+      break;
+    case 6: 
+      strcpy(weekday,"Saturday");
+    default:
+      strcpy(weekday,"Monday - Friday");
+  }
+  clock_copy_time_string(time,sizeof(time));
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Returned week day: %s", weekday);
+  APP_LOG(APP_LOG_LEVEL_DEBUG, "Returned time: %s", time);
+  // SEND IT
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+  dict_write_cstring(iter, 10, weekday);
+  dict_write_cstring(iter, 11, time);
+  app_message_outbox_send();
+} 
+  
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
   // Get the first pair
   Tuple *t = dict_read_first(iterator);
 
   // Process all pairs present
   while (t != NULL) {
-    // Long lived buffer
-    static char s_buffer[64];
 
     // Process this pair's key
     switch (t->key) {
-      case KEY_DATA:
+      case 0:
         // Copy value and display
-        snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", t->value->cstring);
-        text_layer_set_text(s_output_layer, s_buffer);
+        snprintf(stops[0], sizeof(stops[0]), "%s", t->value->cstring);
+        break;
+      case 1:
+        // Copy value and display
+        snprintf(stops[1], sizeof(stops[1]), "%s", t->value->cstring);
+        break;
+      case 2:
+        // Copy value and display
+        snprintf(stops[2], sizeof(stops[2]), "%s", t->value->cstring);
+        break;
+      case 3:
+        // Copy value and display
+        snprintf(stops[3], sizeof(stops[3]), "%s", t->value->cstring);
+        break;
+      case 4:
+        // Copy value and display
+        snprintf(stops[4], sizeof(stops[4]), "%s", t->value->cstring);
+        break;
+      case 5:
+        // Copy value and display
+        snprintf(sched[0], sizeof(sched[0]), "%s", t->value->cstring);
+        break;
+      case 6:
+        // Copy value and display
+        snprintf(sched[1], sizeof(sched[1]), "%s", t->value->cstring);
+        break;
+      case 7:
+        // Copy value and display
+        snprintf(sched[2], sizeof(sched[2]), "%s", t->value->cstring);
+        break;
+      case 8:
+        // Copy value and display
+        snprintf(sched[3], sizeof(sched[3]), "%s", t->value->cstring);
+        break;
+      case 9:
+        // Copy value and display
+        snprintf(sched[4], sizeof(sched[4]), "%s", t->value->cstring);
         break;
     }
 
@@ -54,8 +117,8 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
 
 // MENU WINDOW---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-static void test() {
-  
+static void open_stop() {
+  window_stack_push(stop_window, true);
 }
 
 static void menu_window_load(Window *window) {
@@ -66,8 +129,8 @@ static void menu_window_load(Window *window) {
   int i = 0;
   for (; i < 5; i++) {
     items[i] = (SimpleMenuItem) {
-      .title = "Item",
-      .callback = test
+      .title = stops[i],
+      .callback = open_stop
     };
   }
   
@@ -85,6 +148,27 @@ static void menu_window_load(Window *window) {
 
 static void menu_window_unload(Window *window) {
   simple_menu_layer_destroy(menu_layer);
+}
+
+
+// STOP WINDOW---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+// Stop screen load
+static void stop_window_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect window_bounds = layer_get_bounds(window_layer);
+  
+  stop_text_layer = text_layer_create(window_bounds);
+  text_layer_set_font(stop_text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
+  text_layer_set_text(stop_text_layer, "HALLEFUCKINLUJAH");
+  text_layer_set_overflow_mode(stop_text_layer, GTextOverflowModeWordWrap);
+  text_layer_set_text_alignment(stop_text_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(stop_text_layer));
+}
+
+// Stop screen unload
+static void stop_window_unload(Window *window) {
+  text_layer_destroy(stop_text_layer);
 }
 
 // MAIN WINDOW---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -109,7 +193,7 @@ static void main_window_load(Window *window) {
   // Create output TextLayer
   s_output_layer = text_layer_create(GRect(5, 0, window_bounds.size.w - 5, window_bounds.size.h));
   text_layer_set_font(s_output_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24));
-  text_layer_set_text(s_output_layer, "Welcome to the TCAT App...");
+  text_layer_set_text(s_output_layer, "Welcome to the TCAT App!\n\nPress Any Key to Continue");
   text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
   text_layer_set_text_alignment(s_output_layer,GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
@@ -119,6 +203,10 @@ static void main_window_unload(Window *window) {
   // Destroy output TextLayer
   text_layer_destroy(s_output_layer);
 }
+
+
+// INIT STUFF----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 
 static void init() {
   // Register callbacks
@@ -138,12 +226,22 @@ static void init() {
   });
   window_set_click_config_provider(s_main_window, main_click_config_provider);
   
-  // Create menu Windows
+  // Create menu Window
   menu_window = window_create();
   window_set_window_handlers(menu_window, (WindowHandlers) {
     .load = menu_window_load,
     .unload = menu_window_unload
   });
+  
+  // Create stop Window
+  stop_window = window_create();
+  window_set_window_handlers(stop_window, (WindowHandlers) {
+    .load = stop_window_load,
+    .unload = stop_window_unload
+  });
+  
+  // Request Bus Data by giving time and day
+  request_bus_data();
   
   // Start at main window
   window_stack_push(s_main_window, true);
@@ -153,6 +251,7 @@ static void deinit() {
   // Destroy main Window
   window_destroy(s_main_window);
   window_destroy(menu_window);
+  window_destroy(stop_window);
 }
 
 int main(void) {
